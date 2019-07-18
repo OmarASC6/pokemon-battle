98cc68ac66fb39530f5813a2a1baf2704e5dc524
OmarASC6 <omartarekfawzy@gmail.com>
2019-07-18 14:03:30 -0400

    first commit

--- sketch.js
+++ sketch.js
@@ -2,10 +2,13 @@ function setup(){
     createCanvas(1200,700)
     background(102, 101, 101)
 }
-let green = 217
-let red = 42
-let blue = 120
+let greenL = 217
+let redL = 42
+let blueL = 120
 
+let greenR = 217
+let redR = 42
+let blueR = 120
 
 function draw(){
     // Players Fire Button
@@ -17,19 +20,33 @@ function draw(){
         circle (150,325,100)
         circle (1050,325,100)
     // Health Bars
-        fill (red, green, blue) 
-        leftRect = rect (0,0,250,35)
-        rightRect = rect (950,0,250,35)
+        // fill (redL, greenL, blueL) 
+        // leftRect = rect (0,0,250,35)
+        fill (redL, greenL, blueL) 
+                leftRect0 = rect(0,0,62.5,35);
+                leftRect1 = rect(62.5,0,62.5,35)
+                leftRect2 = rect(125,0,62.5,35)
+                leftRect3 = rect(187.5,0,62.5,35)
+        fill (redR,greenR,blueR)
+        // rightRect = rect (950,0,250,35)
+                rightRect0 = rect(1150,0,62.5,35);
+                rightRect1 = rect(1087.5,0,62.5,35)
+                rightRect2 = rect(1025,0,62.5,35)
+                rightRect3 = rect(962.5,0,62.5,35)
 }
-        //   function mouseClicked(){
-        //      if (mouseX>=0 && mouseX<=50 && mouseY>=300 && mouseY<=350)
-        //      red =255
-        //      green =0
-        //      blue =0
-        //      else if(mouseX>0 && mouseX<50 && mouseY>300 && mouseY<350)
-        //  }
-
-
+          function mouseClicked(){
+              // left box click
+             if (mouseX>=0 && mouseX<=50 && mouseY>=300 && mouseY<=350){
+             redR =255
+             greenR =0
+             blueR =0
+            }
+             else if (mouseX>=1150 && mouseX<=1200 && mouseY>=300 && mouseY<=350){
+             redL = 255
+             greenL = 0
+             blueL = 0 
+            } 
+        }
 
     // // Score Box
     // fill (18, 12, 64) 
@@ -37,5 +54,4 @@ function draw(){
     // // Begin/Reset Button
     // fill (6, 113, 143)
     //     rect (550,150,50,25) 
-
 
