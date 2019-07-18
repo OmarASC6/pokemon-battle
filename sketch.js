964f7b50499b158ece02a4eb162e9ce0cf05e6f9
OmarASC6 <omartarekfawzy@gmail.com>
2019-07-18 14:44:11 -0400

    first commit

--- sketch.js
+++ sketch.js
@@ -2,56 +2,50 @@ function setup(){
     createCanvas(1200,700)
     background(102, 101, 101)
 }
-let greenL = 217
-let redL = 42
-let blueL = 120
+// let greenL = 217
+// let redL = 42
+// let blueL = 120
 
-let greenR = 217
-let redR = 42
-let blueR = 120
-
+// let greenR = 217
+// let redR = 42
+// let blueR = 120
 function draw(){
     // Players Fire Button
     fill (0);
-        square (0,300,50)
-        square (1150,300,50)
+        square (0,300,50)  // Left PLayer Shoot
+        square (1150,300,50) // Right PLayer Shoot
     // Pokemons circles
     fill (204, 20, 54)
         circle (150,325,100)
         circle (1050,325,100)
     // Health Bars
-        // fill (redL, greenL, blueL) 
-        // leftRect = rect (0,0,250,35)
-        fill (redL, greenL, blueL) 
-                leftRect0 = rect(0,0,62.5,35);
-                leftRect1 = rect(62.5,0,62.5,35)
-                leftRect2 = rect(125,0,62.5,35)
-                leftRect3 = rect(187.5,0,62.5,35)
-        fill (redR,greenR,blueR)
-        // rightRect = rect (950,0,250,35)
-                rightRect0 = rect(1150,0,62.5,35);
-                rightRect1 = rect(1087.5,0,62.5,35)
-                rightRect2 = rect(1025,0,62.5,35)
-                rightRect3 = rect(962.5,0,62.5,35)
+    let widthLeft = 250
+    let widthRight = 250
+        fill (0)
+            leftRectGray = rect(0,0,250,35) // Grey Rect Background Left
+        fill (42,217,120) 
+            leftRect = rect (0,0,widthLeft,35) // Green Rect Above Left
+
+        fill (0) 
+            rightRectGray = rect(950,0,250,35) // Grey Rect Background Right
+        fill (42,217,120)
+            rightRect = rect (950,0,widthRight,35) // Green Rect Above Right 
+
 }
           function mouseClicked(){
               // left box click
              if (mouseX>=0 && mouseX<=50 && mouseY>=300 && mouseY<=350){
-             redR =255
-             greenR =0
-             blueR =0
+                widthRight = 25 
+            //  redR =255
+            //  greenR =0
+            //  blueR =0
+
+
             }
              else if (mouseX>=1150 && mouseX<=1200 && mouseY>=300 && mouseY<=350){
-             redL = 255
-             greenL = 0
-             blueL = 0 
+            //  redL = 255
+            //  greenL = 0
+            //  blueL = 0 
             } 
         }
-
-    // // Score Box
-    // fill (18, 12, 64) 
-    //     rect (450,50,250,100)
-    // // Begin/Reset Button
-    // fill (6, 113, 143)
-    //     rect (550,150,50,25) 
 
